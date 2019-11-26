78d9d175ca5dd2b9cb753b4af48061bdc12e86d6
AnkithMohan95 <ankith.mohan@gmail.com>
2019-11-26 15:03:58 -0800

    For visualization

--- pommerman/fight.py
+++ pommerman/fight.py
@@ -25,25 +25,25 @@ import numpy as np
 
 from pommerman import helpers
 from pommerman import make
-#from .. import helpers
-#from .. import make
 from pommerman import utility
 
 
-def run(args, seed=None):
+def run(config,
+        agents_list,
+        record_pngs_dir = None,
+        record_json_dir = None,
+        agent_env_vars = "",
+        game_state_file = None,
+        render_mode = 'human',
+        do_sleep = True,
+        render = False,
+        num_episodes=1,
+        seed=None):
     '''Wrapper to help start the game'''
-    config = args.config
-    record_pngs_dir = args.record_pngs_dir
-    record_json_dir = args.record_json_dir
-    agent_env_vars = args.agent_env_vars
-    game_state_file = args.game_state_file
-    render_mode = args.render_mode
-    do_sleep = args.do_sleep
-    num_times = int(args.num_times)
 
     agents = [
         helpers.make_agent_from_string(agent_string, agent_id)
-        for agent_id, agent_string in enumerate(args.agents.split(','))
+        for agent_id, agent_string in enumerate(agents_list.split(','))
     ]
 
     env = make(config, agents, game_state_file, render_mode=render_mode)
@@ -60,19 +60,19 @@ def run(args, seed=None):
         done = False
 
         while not done:
-            if args.render:
+            if render:
                 env.render(
                     record_pngs_dir=record_pngs_dir,
                     record_json_dir=record_json_dir,
                     do_sleep=do_sleep)
-            if args.render is False and record_json_dir:
+            if render is False and record_json_dir:
                 env.save_json(record_json_dir)
                 time.sleep(1.0 / env._render_fps)
             actions = env.act(obs)
             obs, reward, done, info = env.step(actions)
 
         print("Final Result: ", info)
-        if args.render:
+        if render:
             env.render(
                 record_pngs_dir=record_pngs_dir,
                 record_json_dir=record_json_dir,
@@ -80,14 +80,14 @@ def run(args, seed=None):
             if do_sleep:
                 time.sleep(5)
             env.render(close=True)
-            
-        if args.render is False and record_json_dir:
+
+        if render is False and record_json_dir:
             env.save_json(record_json_dir)
             time.sleep(1.0 / env._render_fps)
 
         if record_json_dir:
             finished_at = datetime.now().isoformat()
-            _agents = args.agents.split(',')
+            _agents = agents_list.split(',')
             utility.join_json_state(record_json_dir, _agents, finished_at,
                                     config, info)
 
@@ -102,7 +102,7 @@ def run(args, seed=None):
 
     infos = []
     times = []
-    for i in range(num_times):
+    for i in range(num_episodes):
         start = time.time()
 
         record_pngs_dir_ = record_pngs_dir + '/%d' % (i+1) \
@@ -118,67 +118,63 @@ def run(args, seed=None):
     return infos
 
 
-def main():
-    '''CLI entry pointed used to bootstrap a battle'''
-    simple_agent = 'test::agents.SimpleAgent'
-    player_agent = 'player::arrows'
-    docker_agent = 'docker::pommerman/simple-agent'
-
-    parser = argparse.ArgumentParser(description='Playground Flags.')
-    parser.add_argument(
-        '--config',
-        default='PommeFFACompetition-v0',
-        help='Configuration to execute. See env_ids in '
-        'configs.py for options.')
-    parser.add_argument(
-        '--agents',
-        default=','.join([simple_agent] * 4),
-        # default=','.join([player_agent] + [simple_agent]*3]),
-        # default=','.join([docker_agent] + [simple_agent]*3]),
-        help='Comma delineated list of agent types and docker '
-        'locations to run the agents.')
-    parser.add_argument(
-        '--agent_env_vars',
-        help='Comma delineated list of agent environment vars '
-        'to pass to Docker. This is only for the Docker Agent.'
-        " An example is '0:foo=bar:baz=lar,3:foo=lam', which "
-        'would send two arguments to Docker Agent 0 and one '
-        'to Docker Agent 3.',
-        default="")
-    parser.add_argument(
-        '--record_pngs_dir',
-        default=None,
-        help='Directory to record the PNGs of the game. '
-        "Doesn't record if None.")
-    parser.add_argument(
-        '--record_json_dir',
-        default=None,
-        help='Directory to record the JSON representations of '
-        "the game. Doesn't record if None.")
-    parser.add_argument(
-        "--render",
-        default=False,
-        action='store_true',
-        help="Whether to render or not. Defaults to False.")
-    parser.add_argument(
-        '--render_mode',
-        default='human',
-        help="What mode to render. Options are human, rgb_pixel, and rgb_array")
-    parser.add_argument(
-        '--game_state_file',
-        default=None,
-        help="File from which to load game state.")
-    parser.add_argument(
-        '--do_sleep',
-        default=True,
-        help="Whether we sleep after each rendering.")
-    parser.add_argument(
-        '--num_times',
-        default=1,
-        help="Number of times you want the game to be played.")
-    args = parser.parse_args()
-    run(args)
-
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     '''CLI entry pointed used to bootstrap a battle'''
+#     simple_agent = 'test::agents.SimpleAgent'
+#     player_agent = 'player::arrows'
+#     docker_agent = 'docker::pommerman/simple-agent'
+#
+#     parser = argparse.ArgumentParser(description='Playground Flags.')
+#     parser.add_argument(
+#         '--config',
+#         default='PommeFFACompetition-v0',
+#         help='Configuration to execute. See env_ids in '
+#         'configs.py for options.')
+#     parser.add_argument(
+#         '--agents',
+#         default=','.join([simple_agent] * 4),
+#         # default=','.join([player_agent] + [simple_agent]*3]),
+#         # default=','.join([docker_agent] + [simple_agent]*3]),
+#         help='Comma delineated list of agent types and docker '
+#         'locations to run the agents.')
+#     parser.add_argument(
+#         '--agent_env_vars',
+#         help='Comma delineated list of agent environment vars '
+#         'to pass to Docker. This is only for the Docker Agent.'
+#         " An example is '0:foo=bar:baz=lar,3:foo=lam', which "
+#         'would send two arguments to Docker Agent 0 and one '
+#         'to Docker Agent 3.',
+#         default="")
+#     parser.add_argument(
+#         '--record_pngs_dir',
+#         default=None,
+#         help='Directory to record the PNGs of the game. '
+#         "Doesn't record if None.")
+#     parser.add_argument(
+#         '--record_json_dir',
+#         default=None,
+#         help='Directory to record the JSON representations of '
+#         "the game. Doesn't record if None.")
+#     parser.add_argument(
+#         "--render",
+#         default=False,
+#         action='store_true',
+#         help="Whether to render or not. Defaults to False.")
+#     parser.add_argument(
+#         '--render_mode',
+#         default='human',
+#         help="What mode to render. Options are human, rgb_pixel, and rgb_array")
+#     parser.add_argument(
+#         '--game_state_file',
+#         default=None,
+#         help="File from which to load game state.")
+#     parser.add_argument(
+#         '--do_sleep',
+#         default=True,
+#         help="Whether we sleep after each rendering.")
+#     args = parser.parse_args()
+#     run(args)
+#
+#
+# if __name__ == "__main__":
+#     main()
